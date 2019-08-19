3289d77b9e6a61199aee6690b2ff94f14af09b04
Suzi Rubino <suzi@suzirubi.com>
2019-08-18 18:24:24 -0700

    refactored forEach into for loop

--- js/scripts.js
+++ js/scripts.js
@@ -5,49 +5,52 @@ var total = "";
 var totals = [];
 
 var countUp = function(number1, number2) {
+  if (number2 < number1 ) {
+    return $("ul.list").append("<li class='red'>Please choose a number to stop at that is greater than the number you are counting by</li>");
+  } else {
   for (var index = number1; index <= number2; index += number1 ) {total += index + " "};
 
   totals = total.split(" ");
 
-  totals.reverse();
-  totals.shift();
-  totals.reverse();
+  totals.pop();
 
-  if (totals.length === 0) {
-    return totals = "error";
-  } else {
-    return totals;
+  return totals;
   }
 }
 
-var errorReset = function(){
-  if (totals === "error") {
-    $("input#number1").val("");
-    $("input#number2").val("");
-    return $("ul#list").append("<li>Please pick a count to number that is larger than the count by number!</li>");
+var resetForm = function(){
+  $("input#number1").val("");
+  $("input#number2").val("");
+}
+
+var createList = function(totals){
+  for (var index = 0; index < totals.length; index += 1) {
+    $("ul.list").append("<li>"+totals[index]+"</li>");
   }
 }
 
 
 $(document).ready(function(){
 
+
   $("form#numbers").submit(function(event){
     event.preventDefault();
 
-    $("ul#list").empty();
+    $("ul.list").empty();
+    $("li").empty();
 
     number1 = parseInt($("input#number1").val());
     number2 = parseInt($("input#number2").val());
+
+
 
     countUp(number1, number2);
 
     console.log(totals);
 
-    errorReset();
+    createList(totals);
 
-    totals.forEach(function(total){
-      $("ul#list").append("<li>" + total + "</li>");
-    });
+    resetForm();
 
   });
 
