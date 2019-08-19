888d632cef2b5296e9bc5268ab717def5792aeb0
Suzi Rubino <suzi@suzirubi.com>
2019-08-18 20:50:56 -0700

    Styled form

--- js/scripts.js
+++ js/scripts.js
@@ -5,36 +5,39 @@ var count = [];
 
 var countUp = function(number1, number2) {
   if (number2 < number1 ) {
-    return "Please choose a number to stop at that is greater than the number you are counting by";
+    return count.push("Please choose a number to stop at that is greater than the number you are counting by");
   } else {
   for (var index = number1; index <= number2; index += number1 ) {count.push(index)};
 
   return count;
   }
-}
+};
 
-var resetForm = function(){
-  $("input#number1").val("");
-  $("input#number2").val("");
-}
+// var resetForm = function(){
+//   $("input#number1").val("");
+//   $("input#number2").val("");
+// };
 
 
 $(document).ready(function(){
 
-
   $("form#numbers").submit(function(event){
-    event.preventDefault();
 
-    $("div.result").empty();
-
+    $("div#result").empty();
+    
     number1 = parseInt($("input#number1").val());
     number2 = parseInt($("input#number2").val());
 
+    countUp(number1,number2);
 
-    $("div.result").text(countUp(number1,number2));
+    console.log(count);
 
-    resetForm();
+    $("div#result").text(count);
 
+    // resetForm();
+
+
+    event.preventDefault();
   });
 
 
