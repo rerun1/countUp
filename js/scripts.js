02f016988efc84dda975f1631dac24b79ec9385c
Suzi Rubino <suzi@suzirubi.com>
2019-08-18 19:18:41 -0700

    stopped battling making the appended list item elements empty and output result as text in a div

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,21 @@
 
 var number1 = 0;
 var number2 = 0;
-var total = "";
-var totals = [];
+var count = [];
 
 var countUp = function(number1, number2) {
   if (number2 < number1 ) {
-    return $("ul.list").append("<li class='red'>Please choose a number to stop at that is greater than the number you are counting by</li>");
+    return "Please choose a number to stop at that is greater than the number you are counting by";
   } else {
-  for (var index = number1; index <= number2; index += number1 ) {total += index + " "};
+  for (var index = number1; index <= number2; index += number1 ) {count.push(index)};
 
-  totals = total.split(" ");
-
-  totals.pop();
-
-  return totals;
+  return count;
   }
 }
 
 var resetForm = function(){
   $("input#number1").val("");
   $("input#number2").val("");
-}
-
-var createList = function(totals){
-  for (var index = 0; index < totals.length; index += 1) {
-    $("ul.list").append("<li>"+totals[index]+"</li>");
-  }
 }
 
 
@@ -36,19 +25,13 @@ $(document).ready(function(){
   $("form#numbers").submit(function(event){
     event.preventDefault();
 
-    $("ul.list").empty();
-    $("li").empty();
+    $("div.result").empty();
 
     number1 = parseInt($("input#number1").val());
     number2 = parseInt($("input#number2").val());
 
 
-
-    countUp(number1, number2);
-
-    console.log(totals);
-
-    createList(totals);
+    $("div.result").text(countUp(number1,number2));
 
     resetForm();
 
